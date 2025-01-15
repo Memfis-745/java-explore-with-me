3b8eff9723fd95a932c94c073c58937e412ff2f8
Memfis <Memfis-745@yandex.ru>
2025-01-15 19:55:56 +0300

    Сервис ewm  - 2

--- ewm-service/src/main/java/ru/practicum/event/EventServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/event/EventServiceImpl.java
@@ -147,12 +147,19 @@ public class EventServiceImpl implements EventService {
         List<String> states = params.getStates();
 
         Pageable page = PageRequest.of(params.getFrom() / params.getSize(), params.getSize());
+        List<Event> events = new ArrayList();
+        if (users.equals(null) && categories.equals(null) && states.equals(null)) {
+            events = eventRepository.findAll();
+        } else {
+            events = eventRepository.getAllEventParams(users, states, categories, start, end);
+        }
+
         log.info("Значение from и ensized  на входе в репозиторий  = {}, {}", params.getFrom(), params.getSize());
         //Page<Event> events = eventRepository.getAllEventParams(users, states, categories, start, end, page);
-        List<Event> events = eventRepository.getAllEventParams(users, states, categories, start, end);
+
         log.info("Значение PageEvents//Лист в методе сервис импл  = {}", events);
-        List<Event> eventAll = eventRepository.findAll();
-        log.info("Значение эвентол в методе сервис импл  = {}", eventAll);
+
+
         //List<EventFullDto> eventFullDtoList =  mapEventsToFullDtos(events.toList());
         List<EventFullDto> eventFullDtoList = mapEventsToFullDtos(events);
         eventFullDtoList.stream()
