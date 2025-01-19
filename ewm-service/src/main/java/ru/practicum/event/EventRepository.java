d1d42e51c56fee80abef23fe371fd2054c6b5872
Memfis <Memfis-745@yandex.ru>
2025-01-19 15:12:03 +0300

    Сервис ewm  - 2

--- ewm-service/src/main/java/ru/practicum/event/EventRepository.java
+++ ewm-service/src/main/java/ru/practicum/event/EventRepository.java
@@ -16,11 +16,11 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     Page<Event> findAllByInitiatorId(Long userId, Pageable page);
 
     @Query("""
-            select event FROM Event event WHERE (?users IS NULL
-            OR event.initiator.id IN ?users) AND (?states IS NULL
-            OR event.state IN ?states) AND (?categories IS NULL
-            OR event.category.id IN ?categories) AND (event.eventDate >= ?rangeStart)
-            AND (event.eventDate <= ?rangeEnd)"""
+            select event FROM Event event WHERE (:users IS NULL
+            OR event.initiator.id IN :users) AND (:states IS NULL
+            OR event.state IN :states) AND (:categories IS NULL
+            OR event.category.id IN :categories) AND (event.eventDate >= :rangeStart)
+            AND (event.eventDate <= :rangeEnd)"""
     )
     Page<Event> getAllEventParams(
             List<Long> users,
