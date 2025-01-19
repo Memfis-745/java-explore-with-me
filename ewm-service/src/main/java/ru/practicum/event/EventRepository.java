8bebe1b0030a9dad9138f15cd55da2c4f5a2fd82
Memfis <Memfis-745@yandex.ru>
2025-01-19 14:23:38 +0300

    Сервис ewm  - 2

--- ewm-service/src/main/java/ru/practicum/event/EventRepository.java
+++ ewm-service/src/main/java/ru/practicum/event/EventRepository.java
@@ -16,11 +16,11 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     Page<Event> findAllByInitiatorId(Long userId, Pageable page);
 
     @Query("""
-            select event FROM Event event WHERE (:users IS NULL
-            OR event.initiator.id IN :users) AND (:states IS NULL
-            OR event.state IN :states) AND (:categories IS NULL
-            OR event.category.id IN :categories) AND (event.eventDate >= :rangeStart)
-            AND (event.eventDate <= :rangeEnd)"""
+            select event FROM Event event WHERE (?users IS NULL
+            OR event.initiator.id IN ?users) AND (?states IS NULL
+            OR event.state IN ?states) AND (?categories IS NULL
+            OR event.category.id IN ?categories) AND (event.eventDate >= ?rangeStart)
+            AND (event.eventDate <= ?rangeEnd)"""
     )
     Page<Event> getAllEventParams(
             List<Long> users,
@@ -30,10 +30,10 @@ public interface EventRepository extends JpaRepository<Event, Long> {
             LocalDateTime rangeEnd, Pageable page);
 
     @Query("""
-            select event FROM Event event WHERE event.state= :state
-            AND (:text IS NULL OR (LOWER(event.description) LIKE %:text%
-            OR LOWER(event.annotation) LIKE %:text%)) AND (:paid IS NULL
-            OR event.paid = :paid) AND (event.eventDate >= :rangeStart) AND (event.eventDate <= :rangeEnd)
+            select event FROM Event event WHERE event.state= ?state
+            AND (?text IS NULL OR (LOWER(event.description) LIKE %?text%
+            OR LOWER(event.annotation) LIKE %?text%)) AND (?paid IS NULL
+            OR event.paid = ?paid) AND (event.eventDate >= ?rangeStart) AND (event.eventDate <= ?rangeEnd)
             ORDER BY event.eventDate"""
     )
     Page<Event> getPublicEventsWithFilter(
